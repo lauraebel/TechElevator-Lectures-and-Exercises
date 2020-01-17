c9417f37073f05bd593afbbd57b7703aa1201fb7
Brian Lauvray <lauvray.1@osu.edu>
2020-01-16 20:09:14 -0500

    Added Command Line Programs

---
+++ module-1/05_Command_Line_Programs/lecture-final/CommandLineArguments/EarthToSunWeightConverter.java
@@ -0,0 +1,27 @@
+public class EarthToSunWeightConverter {
+
+	/*
+	 * String[] args contains arguments passed on the command like, for example, for 
+	 * an application like git, if we typed git add -A, then git would be the program and 
+	 * args would contain  args[0] = "add"  and args[1] = "-A".
+	 * 
+	 * This allows the user to give our program commands or feed it data from the terminal
+	 * command line when they run it.
+	 */
+	public static void main(String[] args) {
+		
+		int sunGravity = 28;
+		
+		/*
+		 * How could we display all the arguments passed into the args array?
+		 */
+
+		
+		/*
+		 * How could we use those arguments in our calculator program?
+		 */
+
+
+	}
+
+}
---
+++ module-1/05_Command_Line_Programs/lecture-final/src/main/java/com/techelevator/EarthToSunWeightConverter.java
@@ -0,0 +1,17 @@
+package com.techelevator;
+
+public class EarthToSunWeightConverter {
+
+	/*
+	 * Command line programs usually follow this work flow:   Take Data, Calculate Data, Give Results
+	 * 
+	 * How can we do each of these steps?
+	 */
+	public static void main(String[] args) {
+		
+		int sunGravity = 28;
+
+
+	}
+
+}
