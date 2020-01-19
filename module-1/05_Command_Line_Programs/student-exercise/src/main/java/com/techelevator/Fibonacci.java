162ecc2c8944cc13f8dc1b9c1a862d5187412fab
Laura Ebel <laura.ebel7@gmail.com>
2020-01-19 17:44:45 -0500

    Only Binary Left

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,37 @@
 package com.techelevator;
+import java.util.Scanner;
+
+// a+b=c
+// b+c=d
+// c+d=e
 
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		
+	System.out.print("Please enter a whole number: ");
+	String userInput = in.nextLine();
+	
+    int userInteger = Integer.parseInt(userInput);
+    
+    int a = 0;
+    int b = 1;
 
-	}
+		for (int i = 0; i <= userInteger; i++) {
+			
+			int sum = a + b;
+			a = b;
+			b = sum;
+			
+		if (sum <= userInteger) {
+			System.out.print(sum + " , ");
+		
+		} else {
+			break;
+		}
 
+}
+}
 }
