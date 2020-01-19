5eadcc320c1866aa647d7708ca18f0e2c3101de5
Laura Ebel <laura.ebel7@gmail.com>
2020-01-19 13:17:21 -0500

    Linear Convert Complete

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,47 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
+// m = f * 0.3048
+// f = m * 3.2808399
+// Please enter the length: 58
+// Is the measurement in (m)eter, or (f)eet? f
+// 58f is 17m
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		
+		double meterConversion = 0.3048;
+		double feetConversion = 3.2808399;
 
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a length: " );
+		String userLength = in.nextLine();
+		
+		double yourChosenLength = Double.parseDouble(userLength);
+		
+		
+		System.out.print("Is it in meters (m) or feet (f)? ");
+		String userMOrF = in.nextLine();
+		
+		if (userMOrF.equalsIgnoreCase("F")) {
+		
+		double convertedToMeters = yourChosenLength * meterConversion;
+		System.out.println("Your chosen length of feet converts to " + convertedToMeters + " meters");
+
+		} else {
+		
+		double convertedToFeet = yourChosenLength * feetConversion;
+		System.out.println("Your chosen length of meters converts to " + convertedToFeet + " feet");
+
+		}
+		
+		
+		// System.out.print("Is the measurement in Meters (m), or Feet (f)?: ");
 	}
 
 }
+//		String userInput = in.nextLine();
+
+
--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/TempConvert.java
@@ -1,5 +1,9 @@
 package com.techelevator;
 
+
+// Tf = Tc * 1.8 + 32 - Cel to Far
+// Tc = (Tf - 32) / 1.8 - Far to Cel
+
 public class TempConvert {
 
 	public static void main(String[] args) {
