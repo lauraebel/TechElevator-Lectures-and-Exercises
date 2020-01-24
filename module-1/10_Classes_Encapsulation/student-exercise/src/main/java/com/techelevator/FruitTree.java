d2236ffbec096558844b17b5c82fea99acb2b9d4
Laura Ebel <laura.ebel7@gmail.com>
2020-01-24 16:35:12 -0500

    FruitTree Exercise Complete

--- module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/FruitTree.java
+++ module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/FruitTree.java
@@ -1,5 +1,32 @@
 package com.techelevator;
 
 public class FruitTree {
+	
+	private String typeOfFruit;
+	private int piecesOfFruitLeft;
+		
+	public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
+		this.typeOfFruit = typeOfFruit;
+		this.piecesOfFruitLeft = startingPiecesOfFruit;
+	}
+	
+	public boolean pickFruit(int numberOfPiecesToRemove) {
+		if (numberOfPiecesToRemove > piecesOfFruitLeft) {
+			return false;
+		} else {
+			piecesOfFruitLeft -= numberOfPiecesToRemove;
+			return true;
+		}
+	}
+	
+	public String getTypeOfFruit() {
+		return typeOfFruit;
+	}
+
+	public int getPiecesOfFruitLeft() {
+		return piecesOfFruitLeft;
+	}
+	
+	
 
 }
