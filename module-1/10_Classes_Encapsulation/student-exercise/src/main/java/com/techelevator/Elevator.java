1b384d330e036757b2837db87886e44deb269cc5
Laura Ebel <laura.ebel7@gmail.com>
2020-01-26 15:24:22 -0500

    Need Help on Elevator&TV

--- module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/Elevator.java
+++ module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/Elevator.java
@@ -1,5 +1,68 @@
 package com.techelevator;
-
 public class Elevator {
+	
+	private int currentFloor;
+	private int numberOfFloors;
+	private boolean doorOpen = false;
+	
+	public Elevator(int totalNumberOfFloors) {
+		this.numberOfFloors = totalNumberOfFloors;
+		this.currentFloor = 1;
+	}
+	
+	public void openDoor() {
+	     doorOpen = true;
+	}
+	
+	public void closeDoor() {
+		doorOpen = false;
+	}
+	
+	//if the door is closed, the current floor needs to become the desired floor 
+	//desired floor cannot be above the last floor or below the 1st floor
+	//^^^^ totalNumberOfFloors is how many floors are available to the elevator
+	
+	public void goUp(int desiredFloor) {
+		if (doorOpen) {
+			// if (desiredFloor <= numberOfFloors && desiredFloor > currentFloor) {
+			for (int i = currentFloor; i <= numberOfFloors; i++) {
+				if (currentFloor != desiredFloor) {
+					continue;
+				} else if (currentFloor == desiredFloor) {
+					break;
+				}
+			}
+		} else
+			return;
+	}
+
+	public void goDown(int desiredFloor) {
+		if (doorOpen) {
+			for (int i = currentFloor; i >= 1; i--) {
+				if (currentFloor != desiredFloor) {
+					continue;
+				}
+				if (currentFloor == desiredFloor) {
+					break;
+				} else
+					return;
+			}
+		}
+	}
+	
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+
+	public int getNumberOfFloors() {
+		return numberOfFloors;
+	}
+
+	public boolean isDoorOpen() {
+		return doorOpen;
+	}
+
+	
+	
 
 }
--- module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/Television.java
+++ module-1/10_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/Television.java
@@ -1,5 +1,64 @@
 package com.techelevator;
 
 public class Television {
+	//Default = TV is off, Channel = 3, Volume = 2
+	private int currentChannel = 3;
+	private int currentVolume = 2;
+	private boolean isOn = false;
+	
+	public void turnOff() {
+		isOn = false;
+	}
+	
+	public void turnOn() {
+		isOn = true;
+	
+		}
+	
+	public void changeChannel(int newChannel) {
+		if (isOn) {
+			if (newChannel >=3 && newChannel <= 18) {
+				
+			}
+			
+		} else return;
+		//if on between >=3 and <= 18
+	}
+	
+	public void channelUp() {
+		if (isOn) {
+			currentChannel ++;
+		} 
+		//if on += 1
+		//if >18 set to 3
+	}
+	
+	public void channelDown() {
+		//if on -= 1
+		//if <3 (currentChannel) set to 18
+	}
+	
+	public void raiseVolume() {
+		//if on += 1
+		//<= 10
+	}
+	
+	public void lowerVolume() {
+		//if on -= 1
+		//<=10
+	}
+
+	public int getCurrentChannel() {
+		return currentChannel;
+	}
+
+	public int getCurrentVolume() {
+		return currentVolume;
+	}
+
+	public boolean isOn() {
+		return isOn;
+	}
+	
 
 }
