f7f5fc14157dfa14f89693cc951786c65686b325
Brian Lauvray <lauvray.1@osu.edu>
2020-01-29 19:32:12 -0500

    added Unit Testing

---
+++ module-1/14_Unit_Testing/student-lecture/src/main/java/com/techelevator/Dog.java
@@ -0,0 +1,47 @@
+package com.techelevator;
+
+public class Dog {
+
+	private String name;
+	private String breed;
+	private int age;
+	
+	public Dog(String name, String breed, int age) {
+		this.name = name;
+		this.breed = breed;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (!(obj instanceof Dog)) {
+//			return false;
+//		}
+//		Dog other = (Dog) obj;
+//		if (!this.name.equals(other.getName())) {
+//			return false;
+//		}
+//		if (!this.breed.equals(other.getBreed())) {
+//			return false;
+//		}
+//		if (this.age != other.getAge()) {
+//			return false;
+//		}
+//		return true;
+//	}
+	
+	
+	
+}
