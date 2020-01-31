d4ee266c801af1a0b21087f62336c3a14aa740e8
Laura Ebel <laura.ebel7@gmail.com>
2020-01-31 10:49:35 -0500

    Passed Equilateral Test

--- module-1/15_Test_Driven_Development/student-lecture/src/main/java/com/techelevator/TriangleClassifier.java
+++ module-1/15_Test_Driven_Development/student-lecture/src/main/java/com/techelevator/TriangleClassifier.java
@@ -3,6 +3,12 @@ package com.techelevator;
 public class TriangleClassifier {
 	
 	public String classify(int sideOne, int sideTwo, int sideThree) {
+		if(sideOne == sideTwo && sideOne == sideThree) {
+			return "equilateral";
+		}
+		
+		
+		
 		return null;
 	}
 	
