b7395cf5288c1b89082448d0ee736f34fd430cf3
Brian <brian@techelevator.com>
2020-01-31 10:49:46 -0500

    Passed Equilateral test

--- module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleClassifier.java
+++ module-1/15_Test_Driven_Development/lecture-final/src/main/java/com/techelevator/TriangleClassifier.java
@@ -3,6 +3,11 @@ package com.techelevator;
 public class TriangleClassifier {
 
 	public String classify( int sideOne, int sideTwo, int sideThree ) {
+		
+		if (sideOne == sideTwo && sideOne == sideThree) {
+			return "equilateral";
+		}
+		
 		return null;
 	}
 	
