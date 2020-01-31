6b43ff7668449c49313b78fc3f7cd0453fe8f24f
Laura Ebel <laura.ebel7@gmail.com>
2020-01-31 17:41:51 -0500

    Created Method in KataFizzBuzz

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,40 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
+	//If the number is divisible by 3, convert the number to the string, "Fizz".
+	//If the number is divisible by 5, convert the number to the string, "Buzz".
+	//If the number is divisible by 3 AND 5, convert the number to the string, "FizzBuzz"
+	//For all other numbers between 1 and 100 (inclusive), simply convert the number to a string.
+	//Any number that is not between 1 and 100 (inclusive), convert the number to an empty string.
+	
+	//If the number is divisible by 3, OR contains a 3, convert the number to the string, "Fizz".
+	//If the number is divisible by 5, OR contains a 5, convert the number to the string, "Buzz".
+	//If the number is divisible by 3 AND 5, OR contains a 3 AND 5, 
+	//convert the number to the string, "FizzBuzz"
+	
+	public String kata(int num) {
+		return null;
+	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -1,8 +1,17 @@
 package com.techelevator;
 
 import org.junit.*;
-import static org.junit.Assert.*;
 
+	//If the number is divisible by 3, convert the number to the string, "Fizz".
+	//If the number is divisible by 5, convert the number to the string, "Buzz".
+	//If the number is divisible by 3 AND 5, convert the number to the string, "FizzBuzz"
+	//For all other numbers between 1 and 100 (inclusive), simply convert the number to a string.
+	//Any number that is not between 1 and 100 (inclusive), convert the number to an empty string.
+
+//If the number is divisible by 3, OR contains a 3, convert the number to the string, "Fizz".
+//If the number is divisible by 5, OR contains a 5, convert the number to the string, "Buzz".
+//If the number is divisible by 3 AND 5, OR contains a 3 AND 5, 
+//convert the number to the string, "FizzBuzz"
 public class KataFizzBuzzTest {
 	
 	private KataFizzBuzz fizzbuzz;
@@ -11,7 +20,10 @@ public class KataFizzBuzzTest {
 	public void setup() {
 		fizzbuzz = new KataFizzBuzz();
 	}
-	
+	@Test
+	public void three_is_divisible_by_3_and_returns_fizz() {
+		
+	}
 	
 	
 	
