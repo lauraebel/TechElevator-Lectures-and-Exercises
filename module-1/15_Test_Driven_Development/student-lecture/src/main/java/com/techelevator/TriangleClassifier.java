b736178931540ed8c9993d3ddca2c79907dc5733
Laura Ebel <laura.ebel7@gmail.com>
2020-01-31 10:48:09 -0500

    Test for Identify Equilateral

---
+++ module-1/15_Test_Driven_Development/student-lecture/src/main/java/com/techelevator/TriangleClassifier.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+public class TriangleClassifier {
+	
+	public String classify(int sideOne, int sideTwo, int sideThree) {
+		return null;
+	}
+	
+	
+	
+	
+	
+
+}
--- module-1/15_Test_Driven_Development/student-lecture/src/test/java/com/techelevator/TriangleClassifierTest.java
+++ module-1/15_Test_Driven_Development/student-lecture/src/test/java/com/techelevator/TriangleClassifierTest.java
@@ -1,5 +1,20 @@
 package com.techelevator;
 
+import org.junit.*;
+
 public class TriangleClassifierTest {
+	
+	private TriangleClassifier classifier;
+	
+	@Before
+	public void setup() {
+		classifier = new TriangleClassifier();
+	}
+	
+	@Test
+	public void identify_equilateral_triangle() {
+		String type = classifier.classify(4, 4, 4);
+		Assert.assertEquals("equilateral", type);
+	}
 
 }
