f040ccc2ff354d5a38b4a296a9ee77ed2cb4a799
Laura Ebel <laura.ebel7@gmail.com>
2020-01-31 11:48:12 -0500

    Refactored strings into constants

--- module-1/15_Test_Driven_Development/student-lecture/src/main/java/com/techelevator/TriangleClassifier.java
+++ module-1/15_Test_Driven_Development/student-lecture/src/main/java/com/techelevator/TriangleClassifier.java
@@ -2,23 +2,26 @@ package com.techelevator;
 
 public class TriangleClassifier {
 	
+	private final static String TYPE_INVALID = "invalid";
+	private final static String TYPE_EQUILATERAL = "equilateral";
+	private final static String TYPE_ISOSCELES = "isosceles";
+	private final static String TYPE_SCALENE = "scalene";
+	
 	public String classify(int sideOne, int sideTwo, int sideThree) {
 		if(!isValidTriangle(sideOne, sideTwo, sideThree)) {
-			return "invalid";
+			return TYPE_INVALID;
 		}
 		
 		if(isEquilateralTriangle(sideOne, sideTwo, sideThree)) {
-			return "equilateral";
+			return TYPE_EQUILATERAL;
 		}
 		if(isScaleneTriangle(sideOne, sideTwo, sideThree)) {
-			return "scalene";
+			return TYPE_SCALENE;
 		}
 		if(isIsoscelesTriangle(sideOne, sideTwo, sideThree )) {
-			return "isosceles";
+			return TYPE_ISOSCELES;
 		}
-	
-		
-		return null;
+		return TYPE_INVALID;
 	}
 	
 	private boolean isValidTriangle(int sideOne, int sideTwo, int sideThree) {
