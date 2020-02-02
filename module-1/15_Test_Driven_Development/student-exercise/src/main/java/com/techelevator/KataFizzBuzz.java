0a25138832eb5fa97ba17a33dc4e0eff34f100db
Laura Ebel <laura.ebel7@gmail.com>
2020-02-02 14:58:33 -0500

    Refactored because with additional tests, Divisible by 3 and 5 test failed

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -13,12 +13,12 @@ public class KataFizzBuzz {
 	//convert the number to the string, "FizzBuzz"
 	
 	public String getKata(int num) {
-		if(((num%3 == 0) && (num%5 != 0) && ((!String.valueOf(num).contains("5"))) || ((String.valueOf(num).contains("3"))))) {
+		if ((num%3 == 0) && (num%5 == 0)) {
+		return "FizzBuzz";
+		} else if(((num%3 == 0) && (num%5 != 0) && ((!String.valueOf(num).contains("5"))) || ((String.valueOf(num).contains("3"))))) {
 			return "Fizz";
 		} else if (((num%5 == 0) && (num%3 != 0)) && ((!String.valueOf(num).contains("3"))) || ((String.valueOf(num).contains("5")))) {
 			return "Buzz";
-		} else if ((num%3 == 0) && (num%5 == 0)) {
-			return "FizzBuzz";
 		} else if ((num%3 != 0) && (num%5 != 0) && (num >= 1) && (num <= 100)) {
 			return String.valueOf(num);
 		} else if ((num%3 != 0) && (num%5 != 0) && (num > 100)) {
