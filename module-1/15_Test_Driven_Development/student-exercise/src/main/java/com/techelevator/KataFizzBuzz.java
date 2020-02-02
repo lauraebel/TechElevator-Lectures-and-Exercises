753dbb491a029ebc01278c643e605daad75a6693
Laura Ebel <laura.ebel7@gmail.com>
2020-02-02 14:45:37 -0500

    Not Divisible by but Contains 3 Test Passed

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -13,7 +13,7 @@ public class KataFizzBuzz {
 	//convert the number to the string, "FizzBuzz"
 	
 	public String getKata(int num) {
-		if((num%3 == 0) && (num%5 != 0)) {
+		if(((num%3 == 0) && (num%5 != 0)) || ((String.valueOf(num).contains("3")))) {
 			return "Fizz";
 		} else if ((num%5 == 0) && (num%3 != 0)) {
 			return "Buzz";
