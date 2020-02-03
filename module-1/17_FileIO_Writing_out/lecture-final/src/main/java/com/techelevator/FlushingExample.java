35d0534aa0995fe765dc53ccdb27bc3ebae71e59
Brian Lauvray <lauvray.1@osu.edu>
2020-02-03 18:46:04 -0500

    Added File IO Part 2

---
+++ module-1/17_FileIO_Writing_out/lecture-final/src/main/java/com/techelevator/FlushingExample.java
@@ -0,0 +1,31 @@
+package com.techelevator;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FlushingExample {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		String message = "Test Line\n";
+		
+		String filePath = "flushExample";
+		
+		File file = new File(filePath);
+		
+		try ( PrintWriter printWriter = new PrintWriter (file );
+				BufferedWriter bufferedWriter = new BufferedWriter( printWriter ) ) {
+			
+//				while (true) {
+					bufferedWriter.write( message );
+//					Thread.sleep(500);
+//					bufferedWriter.flush();
+//				}
+		}
+
+	}
+
+}
