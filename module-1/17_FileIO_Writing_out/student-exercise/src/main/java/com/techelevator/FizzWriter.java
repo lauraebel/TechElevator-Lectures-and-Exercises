1177aea6e691cd336ff25ac2f9f8484c657e7061
Laura Ebel <laura.ebel7@gmail.com>
2020-02-04 17:36:44 -0500

    Fixed FizzWriter to include Contains

--- module-1/17_FileIO_Writing_out/student-exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_FileIO_Writing_out/student-exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -17,21 +17,23 @@ public class FizzWriter {
 				for (int num = 1; num <= 300; num++) {
 					bufferedWriter.write(getResult (num) + System.getProperty("line.separator"));
 				}
-			}	
-		}
+			}
+		System.out.println("FizzBuzz.txt has been created");
+	}
+	
 		private static String getResult(int num) {
 			
 			String result = String.valueOf(num);
-
-			if (num % 3 == 0) {
-				result = "Fizz";
-			} else if (num % 5 == 0) {
-				result = "Buzz";
-			} else if (num % 3 == 0 && num % 5 == 0) {
+			
+			if (num % 3 == 0 && num % 5 == 0) {
 				result = "FizzBuzz";
+			} else if ((num % 5 == 0) || (String.valueOf(num).contains("5") && !String.valueOf(num).contains("3"))) {
+				result = "Buzz";
+			} else if ((num % 3 == 0) || (String.valueOf(num).contains("3") && !String.valueOf(num).contains("5"))) {
+				result = "Fizz";
 			}
 			return result;
 		}
-
+		
 	}
 
