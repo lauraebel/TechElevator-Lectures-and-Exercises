bdde39cf402b8e810819eeda2315fe2e80d281fa
Brian <brian@techelevator.com>
2020-02-11 11:46:34 -0500

    Lecture code

--- module-2/02_Aggregate_Functions_and_GROUP_BY/lecture-final/Extra Advanced Examples/SQL-Date-Examples.sql
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/lecture-final/Extra Advanced Examples/SQL-Date-Examples.sql
@@ -6,14 +6,14 @@ select current_date as today
 select to_char(current_date, 'mm/dd/yyyy') as today
 ;
 -- how many days until Christmas this year - subtract two dates to get duration in days
-select '12/25/2019' - current_date as day_until_Christmas
+select '12/25/2020' - current_date as day_until_Christmas
 ;
 -- be aware of result when calculating days between dates() - date() 
 select  date('12/31/2019') - date('12/01/2019')  as days_in_December
 ;
 -- add/subtract intervals to dates
 select to_char((current_date + interval '5 days'), 'mm/dd/yyyy')as five_days_from_now 
-     , to_char((current_date + interval '1 day'),  'mm/dd/yyyy')as one_month_from_now
+     , to_char((current_date + interval '1 month'),  'mm/dd/yyyy')as one_month_from_now
      , to_char((current_date - interval '1 day'),  'mm/dd/yyyy')as yesterdays_date
      , current_date + interval '2 years' + interval '1 month'   as two_years_and_1_month_from_now
 ;
