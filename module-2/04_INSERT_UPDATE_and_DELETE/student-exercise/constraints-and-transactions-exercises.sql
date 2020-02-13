fb0d6eb992ab50f9af79d793a1fcd6d4d1f03c5e
Laura Ebel <laura.ebel7@gmail.com>
2020-02-13 12:20:36 -0500

    Through 5 Done

--- module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
@@ -1,46 +1,64 @@
--- Write queries to return the following:
--- The following changes are applied to the "dvdstore" database.**
+-- Write queries to return the following: (applied to the "dvdstore" database)
 
--- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
+-- 1. Add actors Hampton Avenue and Lisa Byway to the actor table.
+INSERT INTO actor VALUES (201, 'HAMPTON', 'AVENUE');
+INSERT INTO actor VALUES (202, 'LISA', 'BYWAY');
 
--- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
--- ancient Greece", to the film table. The movie was released in 2008 in English.
--- Since its an epic, the run length is 3hrs and 18mins. There are no special
--- features, the film speaks for itself, and doesn't need any gimmicks.
+SELECT * FROM actor;
 
--- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
--- overprotective mother, in the film, "Euclidean PI". Add them to the film.
+-- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in ancient Greece", to the film table. 
+-- The movie was released in 2008 in English. Since it's an epic, the run length is 3hrs and 18mins. There are no 
+-- special features, the film speaks for itself and doesn't need any gimmicks.
+INSERT INTO film (title, description, release_year, language_id, length) VALUES ('EUCLIDEAN PI', 'The epic story of Euclid as a pizza delivery boy in ancient Greece',
+2008, 1, 198); 
+
+SELECT * FROM film WHERE title = 'EUCLIDEAN PI';
+
+-- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly overprotective mother, in the film, 
+-- "Euclidean PI". Add them to the film.
+INSERT INTO film_actor VALUES (201, 1001);
+INSERT INTO film_actor VALUES (202, 1001);
+
+SELECT * FROM film_actor WHERE film_id = 1001;
 
 -- 4. Add Mathmagical to the category table.
+INSERT INTO category (name) VALUES ('Mathmagical');
 
--- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
--- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+SELECT * FROM category;
 
--- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
--- accordingly.
+-- 5. Assign the Mathmagical category to the following films, "Euclidean PI", "EGG IGBY", "KARATE MOON", 
+-- "RANDOM GO", and "YOUNG LANGUAGE"
+INSERT INTO film_category VALUES (274, 17); 
+INSERT INTO film_category VALUES (494, 17); 
+INSERT INTO film_category VALUES (714, 17); 
+INSERT INTO film_category VALUES (996, 17); 
+INSERT INTO film_category VALUES (1001, 17); 
+
+
+SELECT * FROM film_category WHERE category_id = 17;
+
+-- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films accordingly.
 -- (5 rows affected)
+
 
 -- 7. Add a copy of "Euclidean PI" to all the stores.
 
--- 8. The Feds have stepped in and have impounded all copies of the pirated film,
--- "Euclidean PI". The film has been seized from all stores, and needs to be
--- deleted from the film table. Delete "Euclidean PI" from the film table.
--- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
 
--- 9. Delete Mathmagical from the category table.
+-- 8. The Feds have stepped in and have impounded all copies of the pirated film, "Euclidean PI". The film has been 
+-- seized from all stores, and needs to be deleted from the film table. Delete "Euclidean PI" from the film table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
 
--- 10. Delete all links to Mathmagical in the film_category tale.
--- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
 
--- 11. Retry deleting Mathmagical from the category table, followed by retrying
--- to delete "Euclidean PI".
+-- 9. Delete Mathmagical from the category table. (Did it succeed? Why?)
+
+
+-- 10. Delete all links to Mathmagical in the film_category tale. (Did it succeed? Why?)
+
+
+-- 11. Retry deleting Mathmagical from the category table, followed by retrying to delete "Euclidean PI".
 -- (Did either deletes succeed? Why?)
--- <YOUR ANSWER HERE>
 
--- 12. Check database metadata to determine all constraints of the film id, and
--- describe any remaining adjustments needed before the film "Euclidean PI" can
--- be removed from the film table.
+
+
+
+
--- module-2/04_INSERT_UPDATE_and_DELETE/student-lecture/lecture.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-lecture/lecture.sql
@@ -1,38 +1,69 @@
 -- INSERT
 
 -- 1. Add Klingon as a spoken language in the USA
--- 2. Add Klingon as a spoken language in Great Britain
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+VALUES ('USA', 'Klingon', false, 0);
 
+-- 2. Add Klingon as a spoken language in Great Britain
+INSERT INTO countrylanguage VALUES ('GBR', 'Klingon', false, 1);
+
+SELECT * FROM countrylanguage WHERE language = 'Klingon';
 
 -- UPDATE
+SELECT * FROM city WHERE name = 'Houston'; --3796
+SELECT * FROM country WHERE code = 'USA'; --capital code = 3813
 
 -- 1. Update the capital of the USA to Houston
--- 2. Update the capital of the USA to Washington DC and the head of state
+UPDATE country
+SET capital = 3796
+WHERE code = 'USA';
 
+-- Does the same thing as above ->
+UPDATE country
+SET capital = (SELECT id FROM city WHERE name = 'Houston')
+WHERE code = 'USA';
+
+-- 2. Update the capital of the USA to Washington DC and the head of state
+UPDATE country
+SET capital = 3813, headofstate = 'John Fulton'
+WHERE code = 'USA';
 
 -- DELETE
+SELECT * FROM countrylanguage WHERE language = 'English' AND countrycode = 'USA';
 
 -- 1. Delete English as a spoken language in the USA
--- 2. Delete all occurrences of the Klingon language 
+DELETE FROM countrylanguage
+WHERE language = 'English' AND countrycode = 'USA';
 
+-- 2. Delete all occurrences of the Klingon language 
+SELECT * FROM countrylanguage WHERE language = 'Klingon';
+
+DELETE FROM countrylanguage
+WHERE language = 'Klingon';
 
 -- REFERENTIAL INTEGRITY
 
 -- 1. Try just adding Elvish to the country language table.
+INSERT INTO countrylanguage (language) VALUES ('Elvish');
 
 -- 2. Try inserting English as a spoken language in the country ZZZ. What happens?
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage) VALUES ('ZZZ', 'English', false, 0);
 
 -- 3. Try deleting the country USA. What happens?
-
+DELETE FROM country WHERE code = 'USA';
 
 -- CONSTRAINTS
 
 -- 1. Try inserting English as a spoken language in the USA
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+VALUES ('USA', 'English', true, 70);
 
 -- 2. Try again. What happens?
+-- No longer unique so there is a unique constraint violation
 
 -- 3. Let's relocate the USA to the continent - 'Outer Space'
-
+UPDATE country SET continent = 'Outer Space' WHERE code = 'USA';
+-- There's a list of allowed continents and Outer Space is not on it
 
 -- How to view all of the constraints
 SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS;
@@ -40,6 +71,7 @@ SELECT * FROM INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE;
 SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS;
 
 -- A Query that shows all in a particular schema, which for us is "public"
+-- Taken from Stackoverflow
 SELECT conrelid::regclass AS table_from
      , conname
      , pg_get_constraintdef(c.oid)
@@ -49,12 +81,25 @@ WHERE  contype IN ('f', 'p', 'c', 'u')  -- f=foreign key, p=primary key, c=check
 AND    n.nspname = 'public'
 ORDER  BY conrelid::regclass::text, contype DESC;
 
-
-
 -- TRANSACTIONS
 
 -- 1. Try deleting all of the rows from the country language table and roll it back.
+START TRANSACTION;
+DELETE FROM countrylanguage;
+ROLLBACK; --reverts things to the way they were before the transaction
+-- COMMIT; saves changes made in the transaction
+
+SELECT * FROM countrylanguage;
 
 -- 2. Try updating all of the cities to be in the USA and roll it back
+START TRANSACTION;
+UPDATE city SET countrycode = 'USA';
+ROLLBACK;
 
--- 3. Demonstrate two different SQL connections trying to access the same table where one happens to be inside of a transaction but hasn't committed yet.
+SELECT * FROM city;
+
+-- 3. Demonstrate two different SQL connections trying to access the same table 
+-- where one happens to be inside of a transaction but hasn't committed yet.
+
+
+
