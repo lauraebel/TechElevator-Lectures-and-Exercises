961b7ced80b2187a89accc15bc24bdb9f69d9252
Brian <brian@techelevator.com>
2020-02-14 11:45:00 -0500

    lecture code

--- module-2/05_Database_Design/lecture-final/dcl-lecture.sql
+++ module-2/05_Database_Design/lecture-final/dcl-lecture.sql
@@ -1,13 +1,16 @@
 -- Create a new User with a Password
-
+CREATE USER carson PASSWORD '123';
 
 -- Grant SELECT ON a table to a user
-
+GRANT SELECT ON person TO carson;
 
 -- Grant INSERT ON a table to a user
-
+GRANT INSERT ON person TO carson;
 
 -- Grant SELECT ON a sequence to a user
-
+GRANT USAGE,SELECT ON SEQUENCE person_person_id_seq TO carson;
 
 -- Revoke SELECT ON a table from a user
+REVOKE SELECT ON person FROM carson;
+
+
