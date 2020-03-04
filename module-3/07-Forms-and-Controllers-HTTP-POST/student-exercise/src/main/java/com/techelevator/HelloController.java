eba2f1d03d2b765c663d48a6186e125b7c0b0d03
Laura Ebel <laura.ebel7@gmail.com>
2020-03-03 19:37:17 -0500

    Needs CSS

--- module-3/07-Forms-and-Controllers-HTTP-POST/student-exercise/src/main/java/com/techelevator/HelloController.java
+++ module-3/07-Forms-and-Controllers-HTTP-POST/student-exercise/src/main/java/com/techelevator/HelloController.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,10 +9,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.techelevator.model.Review;
 import com.techelevator.model.ReviewDao;
 
 @Controller
@@ -18,9 +22,32 @@ public class HelloController {
 	
 	@Autowired
 	private ReviewDao reviewDao;
-
-	@RequestMapping("/homePage")
-	public String showHomePage() {
+	
+	@RequestMapping(path="/homePage", method=RequestMethod.GET)
+	public String showReviews(HttpServletRequest request) {
+		request.setAttribute("reviews", reviewDao.getAllReviews());	
 		return "homePage";
 	}
+	
+	@RequestMapping(path="/createReview", method=RequestMethod.GET)
+	public String createBookReviewForm() {
+		return "addReview";
+	}
+
+	@RequestMapping(path="/reviewInput", method=RequestMethod.POST)
+	public String addReview(@RequestParam(required = true) String username, @RequestParam(required = true) int rating, @RequestParam(required = true) String title, @RequestParam(required = true) String text) {
+	Review review = new Review();
+		review.setUsername(username);
+		review.setRating(rating);
+		review.setTitle(title);
+		review.setText(text);
+		review.setDateSubmitted(LocalDateTime.now());
+		
+		reviewDao.save(review);
+		
+		return "redirect:/homePage";
+	}
+	
+
+
 }
--- module-3/07-Forms-and-Controllers-HTTP-POST/student-exercise/src/main/webapp/WEB-INF/jsp/homePage.jsp
+++ module-3/07-Forms-and-Controllers-HTTP-POST/student-exercise/src/main/webapp/WEB-INF/jsp/homePage.jsp
@@ -1,4 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <%@include file="common/header.jspf"%>
 
 
@@ -6,5 +7,21 @@
 
 <img src="<c:url value="/img/forDummies.png" />" />
 
+		<h1>Reader Reviews</h1>
+			
+			<c:forEach items="${reviews}" var="review">
+			<div class="reviewList"></div>
+				<h3><c:out value="${review.username}" /></h3>
+				<c:forEach begin="1" end="${review.rating}" >
+					<img src="<c:url value="/img/star.png" />"/>
+				</c:forEach>
+				<h4><c:out value="${review.title}" /></h4>
+				<h5><c:out value="${review.text}"/></h5>
+				<p><c:out value="${review.dateSubmitted}" /></p>
+			</div>
+			
+			</c:forEach>
+		
+<p><a href="<c:url value="/createReview" />">Click Here to Leave a Review!</a></p>
 
 <%@include file="common/footer.jspf"%>
