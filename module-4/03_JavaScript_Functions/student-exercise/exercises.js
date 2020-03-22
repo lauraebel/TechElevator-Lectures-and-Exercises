4b012ae80314646ec0ece4e2bf453416a64dcdfc
Laura Ebel <laura.ebel7@gmail.com>
2020-03-22 15:02:12 -0400

    List Showing

--- module-4/03_JavaScript_Functions/student-exercise/exercises.js
+++ module-4/03_JavaScript_Functions/student-exercise/exercises.js
@@ -168,7 +168,6 @@ let unfilteredArray = [1, 2, 3, 4, 5, 6];
  * It should work for strings and numbers.
  */
 /**
- * 
  * @param {string[]|number[]} arr 
  * @returns {string|number} largest
  */
--- module-4/04_DOM/student-exercise/shopping-list/js/shopping-list.js
+++ module-4/04_DOM/student-exercise/shopping-list/js/shopping-list.js
@@ -1,17 +1,37 @@
 // add pageTitle
-
+const pageTitle = 'My Shopping List';
 // add groceries
-
+const groceries = ['bread', 'milk', 'butter', 'eggs', 'cereal', 'pasta', 'chicken', 'coffee', 'rice', 'beans'];
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
-
+function setPageTitle() {
+  const title = document.getElementById('title');
+  title.innerText = pageTitle;
+}
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+const main = document.getElementById('groceries');
+
+groceries.forEach(item => {
+  const container = document.createElement('div');
+  container.classList.add('item');
+
+  addItem(container, item);
+
+  main.insertAdjacentElement('beforeend', container);
+}
+  )
+}
+
+function addItem(parent, item) {
+  const items = document.createElement('h4');
+  items.innerText = item;
+  parent.appendChild(items);
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
