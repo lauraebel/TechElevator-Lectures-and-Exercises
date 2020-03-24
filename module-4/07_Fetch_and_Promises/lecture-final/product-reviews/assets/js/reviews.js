1142732fcd0b6029978fcc0de620a245a2366b94
Brian Lauvray <lauvray.1@osu.edu>
2020-03-24 15:20:23 -0400

    Added Fetch and Promise lecture and Intro to Vue Tutorial

---
+++ module-4/07_Fetch_and_Promises/lecture-final/product-reviews/assets/js/reviews.js
@@ -0,0 +1,9 @@
+/**
+ * This function when invoked will look at an array of reviews
+ * and add it to the page by cloning the #review-template
+ */
+function displayReviews() {
+    console.log("Display Reviews...");
+  
+
+}
