a91725f84e947dc9a4fb25b9eb76bbbe3f35abf7
Brian <brian@techelevator.com>
2020-03-26 11:41:27 -0400

    Lecture Code

---
+++ module-4/08_Introduction_to_Vue/lecture-final/vue-product-reviews/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- 3) Use the components name as a tag to position in pages HTML -->
+    <product-reviews></product-reviews>
+    <!--  Removing Hello World
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+// 1) Import my component
+import ProductReviews from './components/ProductReviews.vue'
+
+// 2) Add the variable name from the import to the components list
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld, 
+    ProductReviews
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
