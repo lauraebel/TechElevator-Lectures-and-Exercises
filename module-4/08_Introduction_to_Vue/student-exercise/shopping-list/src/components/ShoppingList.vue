6dfc0614801715b58b55bd72f062dd9ee5e8b8b5
Laura Ebel <laura.ebel7@gmail.com>
2020-03-30 14:28:46 -0400

    Shopping List Working

---
+++ module-4/08_Introduction_to_Vue/student-exercise/shopping-list/src/components/ShoppingList.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="shopping-list">
+    <h1>{{title}}</h1>
+       <ul>
+        <li>Bread</li>
+        <li>Milk</li>
+        <li>Eggs</li>
+        <li>Butter</li>
+        <li>Cereal</li>
+        <li>Peanut Butter</li>
+        <li>La Croix</li>
+        <li>Rice</li>
+        <li>Pasta</li>
+       </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'shopping-list',
+  data() {
+      return {
+          title: 'My Shopping List'
+      }
+}
+}
+</script>
+
+<style scoped>
+.shopping-list {
+    width:450px;
+    background: #fff;
+    margin: 50px auto;
+    font-family: 'Roboto Condensed', sans-serif;
+    border-radius: 10px;
+}
+h1 {
+    background:#f2f2f2;
+    color:#5c8578;
+    padding:10px;
+    font-size:24px;
+    text-transform: uppercase;
+    text-align: center;
+    margin-bottom: 0px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+}
+ul {
+    list-style-type: none;
+    margin:0px;
+    padding:0px;
+}
+li {
+    font-size: 24px;
+    border-bottom:1px solid #f2f2f2;
+    padding:10px 20px;
+}
+li:last-child{
+    border:0px;
+}
+li.completed {
+    text-decoration: line-through;
+    color:darkgrey;
+}
+</style>
+
