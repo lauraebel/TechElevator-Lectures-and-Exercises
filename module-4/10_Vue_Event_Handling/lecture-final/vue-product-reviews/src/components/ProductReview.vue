15e991398f60f68e9943fae8410e87fd5cf7915e
Brian <brian@techelevator.com>
2020-03-31 11:16:47 -0400

    Leture code

--- module-4/10_Vue_Event_Handling/lecture-final/vue-product-reviews/src/components/ProductReview.vue
+++ module-4/10_Vue_Event_Handling/lecture-final/vue-product-reviews/src/components/ProductReview.vue
@@ -5,48 +5,53 @@
         <p class="description">{{ description }}</p>
 
         <div class="well-display">
-            <div class="well">
+            <div class="well" v-on:click="filter = 0">
                 <span class="amount">{{ averageRating }}</span>
                 Average Rating
             </div>
 
-            <div class="well">
+            <div class="well" v-on:click="filter = 1">
                 <span class="amount">{{ numberOfOneStarReviews }}</span>
                 1 Star Review{{ numberOfOneStarReviews === 1 ? '' : 's' }}
             </div>
 
-            <div class="well">
+            <div class="well" v-on:click="filter = 2">
                 <span class="amount">{{ numberOfTwoStarReviews }}</span>
                 2 Star Review{{ numberOfTwoStarReviews === 1 ? '' : 's' }}
             </div>
 
-            <div class="well">
+            <div class="well" v-on:click="filter = 3">
                 <span class="amount">{{ numberOfThreeStarReviews }}</span>
                 3 Star Review{{ numberOfThreeStarReviews === 1 ? '' : 's' }}
             </div>
 
-            <div class="well">
+            <div class="well" v-on:click="filter = 4">
                 <span class="amount">{{ numberOfFourStarReviews }}</span>
                 4 Star Review{{ numberOfFourStarReviews === 1 ? '' : 's' }}
             </div>
 
-            <div class="well">
+            <div class="well" v-on:click="filter = 5">
                 <span class="amount">{{ numberOfFiveStarReviews }}</span>
                 5 Star Review{{ numberOfFiveStarReviews === 1 ? '' : 's' }}
             </div>
         </div>
 
-        <a href="#">Show Form</a>
+        <a href="#" v-on:click.prevent="showForm = true" v-if="showForm === false">Show Form</a>
 
-        <form>
+        <form v-on:submit.prevent="addNewReview" v-if="showForm === true">
             <div class="form-element">
-                <label for="reviewer">Name:</label><input id="reviewer" type="text"  />
+                <label for="reviewer">Name:</label>
+                <input id="reviewer" type="text" 
+                    v-model.trim="newReview.reviewer" />
             </div>
             <div class="form-element">
-                <label for="title">Title:</label> <input id="title" type="text" />
+                <label for="title">Title:</label> 
+                <input id="title" type="text"
+                    v-model.trim="newReview.title" />
             </div>
             <div class="form-element">
-                <label for="rating">Rating:</label> <select id="rating">
+                <label for="rating">Rating:</label> 
+                <select id="rating" v-model.number="newReview.rating">
                     <option value="1">1 Star</option>
                     <option value="2">2 Stars</option>
                     <option value="3">3 Stars</option>
@@ -56,13 +61,13 @@
             </div>
             <div class="form-element">
                 <label for="review">Review</label>
-                <textarea id="review"></textarea>
+                <textarea id="review" v-model.trim="newReview.review"></textarea>
             </div>
-            <button>Submit</button>
-            <button type="cancel">Cancel</button>
+            <button v-bind:disabled="!isFormValid">Submit</button>
+            <button type="cancel" v-on:click.prevent="resetForm">Cancel</button>
         </form>
 
-        <div class="review" v-for="review in reviews" v-bind:key="review.id">
+        <div class="review" v-for="review in filteredReviews" v-bind:key="review.id">
             <h4>{{ review.reviewer }}</h4>
             <div class="rating">
                 <img src="../assets/star.png" v-bind:title="review.rating + ' Star Review'" class="ratingStar" v-for="n in review.rating" v-bind:key="n" />
@@ -81,6 +86,9 @@ export default {
         return {
             name: 'Cigar Parties for Dummies',
             description: 'Host and plan the perfect cigar party for all of your squirrelly friends.',
+            showForm: false,
+            newReview: {},
+            filter: 0,
             reviews: [
                 {
                     reviewer: "Malcolm Gladwell",
@@ -114,7 +122,7 @@ export default {
             let sum = vm.reviews.reduce( (currentSum, review) => {
                 return currentSum + review.rating;
             }, 0);
-            return sum / vm.reviews.length;
+            return (sum / vm.reviews.length).toFixed(2);
         },
         numberOfOneStarReviews(vm) {
             return vm.numberOfReviews(vm.reviews, 1);
@@ -131,13 +139,32 @@ export default {
         numberOfFiveStarReviews(vm) {
             return vm.numberOfReviews(vm.reviews, 5);
         },
+        isFormValid() {
+            // return true if all of the values in the review are truthy
+            return this.newReview.reviewer && this.newReview.title 
+                && this.newReview.rating && this.newReview.review;
+        },
+        filteredReviews() {
+            const filteredReviews = this.reviews.filter( review => {
+                return this.filter === 0 ? true : this.filter === review.rating;
+            });
+            return filteredReviews;
+        }
     },
     methods: {
         numberOfReviews(reviews, starType) {
             return reviews.reduce( (currentCount, review ) => {
                 return currentCount + ( review.rating === starType ? 1 : 0);
             }, 0);
-        },       
+        },  
+        resetForm() {
+            this.showForm = false;
+            this.newReview = {};
+        },
+        addNewReview() {
+            this.reviews.unshift(this.newReview);
+            this.resetForm();
+        }    
     }
 }
 </script>
