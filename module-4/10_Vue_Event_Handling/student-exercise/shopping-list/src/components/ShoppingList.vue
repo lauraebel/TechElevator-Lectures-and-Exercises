477dc8fc8509799e64728a6685f314230dcd0344
Laura Ebel <laura.ebel7@gmail.com>
2020-04-01 21:50:00 -0400

    Vue Event Handling Exercises Complete

--- module-4/10_Vue_Event_Handling/student-exercise/shopping-list/src/components/ShoppingList.vue
+++ module-4/10_Vue_Event_Handling/student-exercise/shopping-list/src/components/ShoppingList.vue
@@ -2,10 +2,10 @@
     <div class="shopping-list">
         <h1>My Shopping List</h1>
         <ul>
-            <li v-for="item in groceries" v-bind:key="item.id" :class="{ completed: item.completed }" v-on:click="changeStatus(item.id)">
+            <li v-for="item in groceries" v-bind:key="item.id" :class="{completed: item.completed}" v-on:click="changeStatus(item.id)">
                 <input type="checkbox" :checked="item.completed"/>
                 {{item.name}} 
-                <i class="far fa-check-circle" :class="{ completed: item.completed }"></i>
+                <i class="far fa-check-circle" :class="{completed: item.completed}"></i>
             </li>
         </ul>
     </div>
@@ -16,15 +16,15 @@ export default {
     data() {
         return {
             groceries: [
-                {id: 1, name: 'Oatmeal', completed: false },
-                {id: 2, name: 'Milk', completed: false },
-                {id: 3, name: 'Banana', completed: false },
-                {id: 4, name: 'Strawberries', completed: false },
-                {id: 5, name: 'Lunch Meat', completed: false },
-                {id: 6, name: 'Bread', completed: false },
-                {id: 7, name: 'Grapes', completed: false },
-                {id: 8, name: 'Steak', completed: false },
-                {id: 9, name: 'Salad', completed: false }
+                {id: 1, name: 'Oatmeal', completed: false},
+                {id: 2, name: 'Milk', completed: false},
+                {id: 3, name: 'Banana', completed: false},
+                {id: 4, name: 'Strawberries', completed: false},
+                {id: 5, name: 'Lunch Meat', completed: false},
+                {id: 6, name: 'Bread', completed: false},
+                {id: 7, name: 'Grapes', completed: false},
+                {id: 8, name: 'Steak', completed: false},
+                {id: 9, name: 'Salad', completed: false}
             ]
         }
     },
