c2718fd8d5672fa4e9c9ef4223707be1a34528b1
Brian <brian@techelevator.com>
2020-04-02 11:44:00 -0400

    Lecture code

--- module-4/12_Vue_Router/lecture-final/user-management/src/router.js
+++ module-4/12_Vue_Router/lecture-final/user-management/src/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Users from './views/Users.vue'
+import User from './views/User.vue'
 
 Vue.use(Router)
 
@@ -7,6 +9,19 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-
+    {
+      path: '/',
+      redirect: { name: 'users' }
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: Users
+    },
+    {
+      path: '/user/:id',
+      name: 'user',
+      component: User
+    }
   ]
 })
---
+++ module-4/12_Vue_Router/lecture-final/user-management/src/views/User.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="user-profile">
+        <h1>User Profile</h1>
+        <pre>{{ user }}</pre>
+        <router-link :to="{ name: 'users'}">Back to Users List</router-link>
+    </div>
+</template>
+
+<script>
+import userService from '@/service/UserService';
+
+export default {
+    name: 'user-profile',
+    data() {
+        return {
+            user: null
+        }
+    },
+    methods: {
+        getUser(id) {
+            userService.get(id).then( user => this.user = user);
+        }
+    },
+    created() {
+        // get the parameter id from the dynamic data in the url
+        console.log( this.$route.params.id );
+
+        this.getUser( this.$route.params.id );
+    }
+}
+</script>
